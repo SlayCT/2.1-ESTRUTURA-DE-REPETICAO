170e0601a68087a4c0d2236b766d62456c607b09
Cicera Thais <cicerathaisizidio01@gmail.com>
2026-03-19 19:44:33 -0300

    Add files via upload

---
+++ Meses.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Meses {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número (1 a 12) ou 0 para sair: ");
+        int numero = sc.nextInt();
+
+        while (numero != 0) {
+
+            switch(numero) {
+                case 1:
+                    System.out.println("Janeiro");
+                    break;
+                case 2:
+                    System.out.println("Fevereiro");
+                    break;
+                case 3:
+                    System.out.println("Março");
+                    break;
+                case 4:
+                    System.out.println("Abril");
+                    break;
+                case 5:
+                    System.out.println("Maio");
+                    break;
+                case 6:
+                    System.out.println("Junho");
+                    break;
+                case 7:
+                    System.out.println("Julho");
+                    break;
+                case 8:
+                    System.out.println("Agosto");
+                    break;
+                case 9:
+                    System.out.println("Setembro");
+                    break;
+                case 10:
+                    System.out.println("Outubro");
+                    break;
+                case 11:
+                    System.out.println("Novembro");
+                    break;
+                case 12:
+                    System.out.println("Dezembro");
+                    break;
+                default:
+                    System.out.println("Mês inválido");
+            }
+
+            System.out.print("Digite outro número (ou 0 para sair): ");
+            numero = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
